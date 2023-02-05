39d4c945ae9d2859293640e037f942527080d834
brent couillard <brentcouillard@yahoo.com>
2023-02-04 18:22:38 -0600

    commit tree c457f24ad7691ad8bcf249534e900a5d1adea0ec

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-const employee = {
-    name: "",
-    streetAddress: "",
-};
+const employee = {name: "Brent", address: "earth"}
 
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    return Object.assign({}, employee, {[key]:value})
+
+//this function should take in three arguments: an employee Object, a key and a value. This function should not mutate the employee; it should return a new Object that has an updated value for the key passed in. Hint: use the spread operator!
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    }
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+// this function should work the same as updateEmployeeWithKeyAndValue() but it should mutate the employee Object passed in.
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value
     return employee
 }
 
-function deleteFromEmployeeByKey(employee, key){
-    const newObject = Object.assign({}, employee)
+//this function should take in a employee Object and a key. It should delete the property with that key from the employee Object. This should not mutate the original employee Object; it should return a new Object that doesn't include the identified key-value pair. Hint: use the spread operator!
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObject = {...employee}
     delete newObject[key]
     return newObject
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key){
-    delete employee[key]
-    return employee;
+
+
+//this function should work the same as deleteFromEmployeeByKey() but it should mutate the employee Object
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const newObject = employee
+    delete newObject[key]
+    return newObject
 }
